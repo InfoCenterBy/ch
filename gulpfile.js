5b9d79c780a344c69d22260b6517518f1b7fa9bc
dyamorph <dyamorph22@gmail.com>
2026-03-23 17:56:30 +0300

    update: files, styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,71 @@
-let project_folder = require("path").basename(__dirname);
-let source_folder = "src";
+let project_folder = require('path').basename(__dirname);
+let source_folder = 'src';
 
-let fs = require("fs");
-let execSync = require("child_process").execSync;
+let fs = require('fs');
+let execSync = require('child_process').execSync;
 
 let path = {
   build: {
-    html: project_folder + "/",
-    css: project_folder + "/css/",
-    js: project_folder + "/js/",
-    img: project_folder + "/img/",
-    fonts: project_folder + "/fonts/",
-    audio: project_folder + "/audio/",
+    html: project_folder + '/',
+    css: project_folder + '/css/',
+    js: project_folder + '/js/',
+    img: project_folder + '/img/',
+    fonts: project_folder + '/fonts/',
+    audio: project_folder + '/audio/',
   },
   src: {
-    html: [source_folder + "/**/*.html"],
-    css: source_folder + "/css/tailwind.css",
-    js: source_folder + "/js/script.js",
-    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
-    fonts: [source_folder + "/fonts/*.ttf", source_folder + "/fonts/*.woff", source_folder + "/fonts/*.woff2"],
-    audio: source_folder + "/audio/*.mp3",
+    html: [source_folder + '/**/*.html'],
+    css: source_folder + '/css/tailwind.css',
+    js: source_folder + '/js/script.js',
+    img: source_folder + '/img/**/*.{jpg,png,svg,gif,ico,webp}',
+    fonts: [
+      source_folder + '/fonts/*.ttf',
+      source_folder + '/fonts/*.woff',
+      source_folder + '/fonts/*.woff2',
+    ],
+    audio: source_folder + '/audio/*.mp3',
   },
   watch: {
-    html: source_folder + "/**/*.html",
-    css: source_folder + "/css/**/*.css",
-    js: source_folder + "/js/**/*.js",
-    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
-    audio: source_folder + "/audio/*.mp3",
+    html: source_folder + '/**/*.html',
+    css: source_folder + '/css/**/*.css',
+    js: source_folder + '/js/**/*.js',
+    img: source_folder + '/img/**/*.{jpg,png,svg,gif,ico,webp}',
+    audio: source_folder + '/audio/*.mp3',
   },
-  clean: "./" + project_folder + "/",
+  clean: './' + project_folder + '/',
 };
 
-let { src, dest } = require("gulp"),
-  gulp = require("gulp"),
-  browsersync = require("browser-sync").create(),
-  fileinclude = require("gulp-file-include"),
-  del = require("del"),
-  scss = require("gulp-sass")(require("sass")),
-  autoprefixer = require("gulp-autoprefixer"),
+let { src, dest } = require('gulp'),
+  gulp = require('gulp'),
+  browsersync = require('browser-sync').create(),
+  fileinclude = require('gulp-file-include'),
+  del = require('del'),
+  scss = require('gulp-sass')(require('sass')),
+  autoprefixer = require('gulp-autoprefixer'),
   // group_media = require("gulp-group-css-media-queries"),
   // clean_css = require("gulp-clean-css"),
-  lightningcss = require("gulp-lightningcss"),
-  rename = require("gulp-rename"),
+  lightningcss = require('gulp-lightningcss'),
+  rename = require('gulp-rename'),
   // uglify = require("gulp-uglify-es").default,
-  terser = require("gulp-terser"),
+  terser = require('gulp-terser'),
   // postcss = require("gulp-postcss"),
   // cssnano = require("cssnano"),
   // cssVariables = require("postcss-css-variables");
-  ghpages = require("gh-pages"),
-  replace = require("gulp-replace"),
-  cheerio = require("gulp-cheerio");
+  ghpages = require('gh-pages'),
+  replace = require('gulp-replace'),
+  cheerio = require('gulp-cheerio');
 
-((imagemin = require("gulp-imagemin")),
-  (svgSprite = require("gulp-svg-sprite")),
-  (ttf2woff = require("gulp-ttf2woff")),
-  (ttf2woff2 = require("gulp-ttf2woff2")),
-  (fonter = require("gulp-fonter")),
-  (deploy = require("gulp-gh-pages")));
+((imagemin = require('gulp-imagemin')),
+  (svgSprite = require('gulp-svg-sprite')),
+  (ttf2woff = require('gulp-ttf2woff')),
+  (ttf2woff2 = require('gulp-ttf2woff2')),
+  (fonter = require('gulp-fonter')),
+  (deploy = require('gulp-gh-pages')));
 
 function browserSync(done) {
   browsersync.init({
     server: {
-      baseDir: "./" + project_folder + "/",
+      baseDir: './' + project_folder + '/',
     },
     port: 3000,
     notify: false,
@@ -69,7 +73,10 @@ function browserSync(done) {
 }
 
 function html() {
-  return src(path.src.html).pipe(fileinclude()).pipe(dest(path.build.html)).pipe(browsersync.stream());
+  return src(path.src.html)
+    .pipe(fileinclude())
+    .pipe(dest(path.build.html))
+    .pipe(browsersync.stream());
 }
 
 // function css() {
@@ -94,8 +101,8 @@ function html() {
 // }
 
 function css() {
-  return src("src/css/style.css")
-    .pipe(rename({ basename: "style", extname: ".css" })) 
+  return src('src/css/style.css')
+    .pipe(rename({ basename: 'style', extname: '.css' }))
     .pipe(dest(path.build.css))
     .pipe(browsersync.stream());
 }
@@ -112,7 +119,21 @@ function js() {
     )
     .pipe(
       rename({
-        extname: ".min.js",
+        extname: '.min.js',
+      }),
+    )
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream());
+}
+
+function jsVendor() {
+  return src(
+    ['node_modules/qr-scanner/qr-scanner.umd.min.js', 'node_modules/qr-scanner/qr-scanner-worker.min.js'],
+    { base: 'node_modules/qr-scanner' },
+  )
+    .pipe(
+      rename((p) => {
+        p.dirname = '';
       }),
     )
     .pipe(dest(path.build.js))
@@ -144,24 +165,24 @@ function fonts() {
   return src(path.src.fonts).pipe(ttf2woff2()).pipe(dest(path.build.fonts));
 }
 
-gulp.task("otf2ttf", function () {
-  return src([source_folder + "/fonts/*.otf"])
+gulp.task('otf2ttf', function () {
+  return src([source_folder + '/fonts/*.otf'])
     .pipe(
       fonter({
-        formats: ["ttf"],
+        formats: ['ttf'],
       }),
     )
-    .pipe(dest(source_folder + "/fonts/"));
+    .pipe(dest(source_folder + '/fonts/'));
 });
 
-gulp.task("svgSprite", function () {
+gulp.task('svgSprite', function () {
   return gulp
-    .src([source_folder + "/iconsprite/*.svg"])
+    .src([source_folder + '/iconsprite/*.svg'])
     .pipe(
       svgSprite({
         mode: {
           stack: {
-            sprite: "../icons/icons.svg", //sprite file name
+            sprite: '../icons/icons.svg', //sprite file name
             example: true,
           },
         },
@@ -170,8 +191,8 @@ gulp.task("svgSprite", function () {
     .pipe(dest(path.build.img));
 });
 
-gulp.task("deploy", function () {
-  return gulp.src("./skko-redesign/**/*").pipe(deploy());
+gulp.task('deploy', function () {
+  return gulp.src('./skko-redesign/**/*').pipe(deploy());
 });
 
 function cb() {}
@@ -192,49 +213,52 @@ function deployTask(done) {
   // Читаем имя проекта из package.json и формируем репозиторий ИМЯ-demo
   let pkg = {};
   try {
-    pkg = JSON.parse(fs.readFileSync("package.json", "utf8")) || {};
+    pkg = JSON.parse(fs.readFileSync('package.json', 'utf8')) || {};
   } catch (e) {
-    console.warn("Не удалось прочитать package.json, используем имя папки проекта:", e.message);
+    console.warn('Не удалось прочитать package.json, используем имя папки проекта:', e.message);
   }
   const baseName = (pkg && (pkg.description || pkg.name)) || project_folder;
-  const targetRepoName = baseName + "-demo";
+  const targetRepoName = baseName + '-demo';
 
   // Определяем владельца GitHub из origin и сохраняем протокол (ssh/https)
-  let originUrl = "";
+  let originUrl = '';
   try {
-    originUrl = execSync("git config --get remote.origin.url", {
-      encoding: "utf8",
+    originUrl = execSync('git config --get remote.origin.url', {
+      encoding: 'utf8',
     }).trim();
   } catch (e) {
-    console.warn("Не удалось получить origin url из git:", e.message);
+    console.warn('Не удалось получить origin url из git:', e.message);
   }
   let owner = null;
-  let proto = "https";
+  let proto = 'https';
   if (originUrl) {
     const m = originUrl.match(/github\.com[:\/]([^\/]+)\//i);
     owner = m ? m[1] : null;
-    proto = originUrl.startsWith("git@") ? "ssh" : "https";
+    proto = originUrl.startsWith('git@') ? 'ssh' : 'https';
   }
   const targetRepoUrl = owner
-    ? proto === "ssh"
-      ? "git@github.com:" + owner + "/" + targetRepoName + ".git"
-      : "https://github.com/" + owner + "/" + targetRepoName + ".git"
-    : "";
+    ? proto === 'ssh'
+      ? 'git@github.com:' + owner + '/' + targetRepoName + '.git'
+      : 'https://github.com/' + owner + '/' + targetRepoName + '.git'
+    : '';
 
   ghpages.publish(
-    "./" + project_folder,
+    './' + project_folder,
     {
-      branch: "gh-pages",
+      branch: 'gh-pages',
       repo: targetRepoUrl, // Пушим в репозиторий ИМЯ-demo того же владельца
-      message: "Deploy to GitHub Pages - " + new Date().toISOString(),
+      message: 'Deploy to GitHub Pages - ' + new Date().toISOString(),
       force: true, // Принудительно обновляем
       dotfiles: true, // Включаем скрытые файлы
     },
     function (err) {
       if (err) {
-        console.error("Deploy failed:", err);
+        console.error('Deploy failed:', err);
       } else {
-        console.log("Deploy successful! Ветка gh-pages, репозиторий:", targetRepoUrl || "(текущий)");
+        console.log(
+          'Deploy successful! Ветка gh-pages, репозиторий:',
+          targetRepoUrl || '(текущий)',
+        );
       }
       done(err);
     },
@@ -252,14 +276,14 @@ function watchFilesLorem(params) {
 
 // Генерирует строку из "рыбы" заданной длины символов
 function generateLoremByLength(targetLength) {
-  if (!Number.isFinite(targetLength) || targetLength <= 0) return "";
+  if (!Number.isFinite(targetLength) || targetLength <= 0) return '';
   const base = (
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua " +
-    "Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat " +
-    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur " +
-    "Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum "
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ' +
+    'Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat ' +
+    'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur ' +
+    'Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum '
   ).repeat(20); // запас по длине
-  return base.replace(/\s+/g, " ").slice(0, targetLength);
+  return base.replace(/\s+/g, ' ').slice(0, targetLength);
 }
 
 function loremGenerate() {
@@ -267,21 +291,32 @@ function loremGenerate() {
     src(path.src.html)
       .pipe(
         fileinclude({
-          prefix: "@@",
-          basepath: "@file",
+          prefix: '@@',
+          basepath: '@file',
           indent: true,
         }),
       )
-      .on("error", function (err) {
-        console.error("fileinclude error in loremGenerate():", err.message);
-        this.emit("end");
+      .on('error', function (err) {
+        console.error('fileinclude error in loremGenerate():', err.message);
+        this.emit('end');
       })
       // Безопасная DOM-замена текста через cheerio: только текстовые узлы
       .pipe(
         cheerio({
           run: function ($) {
-            const excluded = new Set(["script", "style", "head", "html", "body", "meta", "link", "title", "svg", "noscript"]);
-            $("*").each(function () {
+            const excluded = new Set([
+              'script',
+              'style',
+              'head',
+              'html',
+              'body',
+              'meta',
+              'link',
+              'title',
+              'svg',
+              'noscript',
+            ]);
+            $('*').each(function () {
               const el = this;
               if (excluded.has(el.tagName && el.tagName.toLowerCase())) {
                 return;
@@ -289,15 +324,15 @@ function loremGenerate() {
               $(el)
                 .contents()
                 .filter(function () {
-                  return this.type === "text";
+                  return this.type === 'text';
                 })
                 .each(function () {
                   const original = this.data;
                   if (original && original.trim().length > 0) {
                     const m = original.match(/^(\s*)([\s\S]*?)(\s*)$/);
-                    const leading = m ? m[1] : "";
+                    const leading = m ? m[1] : '';
                     const core = m ? m[2] : original;
-                    const trailing = m ? m[3] : "";
+                    const trailing = m ? m[3] : '';
                     this.data = leading + generateLoremByLength(core.length) + trailing;
                   }
                 });
@@ -311,7 +346,7 @@ function loremGenerate() {
   );
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts, audio));
+let build = gulp.series(clean, gulp.parallel(js, jsVendor, css, html, images, fonts, audio));
 let lorem = gulp.series(build, loremGenerate, gulp.parallel(watchFilesLorem, browserSync));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
